c7df597727ad6d7f8781561bfb85b3a5f0375b4c
Pedrofrancisco2006 <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-04-10 11:02:53 -0300

    Add files via upload

---
+++ Verificador de numeros.py
@@ -0,0 +1,12 @@
+numero = float(input('Digite um numero '))
+if numero  > 0  and numero % 2 == 0 :
+    print("Positivo e Par")
+elif numero > 0 and numero % 3 == 0 :   
+    print("Positivo e Multiplo de três")
+elif numero < 0 and numero % 2 != 0:
+    print("Impar e negativo")    
+elif numero == 0:
+    print("Zero")
+else:
+    print-("Este numero não está relacionado com tais dados descritos a cima")         
+    
