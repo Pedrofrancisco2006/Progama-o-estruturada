f5dde44d55dd6c20f3a6ca48c6b1afb888ff0cb0
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-04-11 08:17:37 -0300

    Update Calculadora de Desconto.py

--- Calculadora de Desconto.py
+++ Calculadora de Desconto.py
@@ -1,4 +1,4 @@
-preco = float(input("Digite o Preço "))
-desconto = float(input("Digite seu desconto "))
+preco = float(input("Digite o Preço: "))
+desconto = float(input("Digite seu desconto: "))
 formula = preco-(preco*desconto/100)
 print("Seu preço atual é de",formula, ".")
