d53ffbc2976eaf574e9adce9eb0141b7f1af482f
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-04-11 08:21:02 -0300

    Update Juros simples.py

--- Juros simples.py
+++ Juros simples.py
@@ -1,5 +1,5 @@
-capital= float(input("digite a capital:"))
-tempo= float(input("digite o tempo:"))
-taxa= float(input("digite a taxa:"))
+capital= float(input("Qual o seu capital inicial?: "))
+tempo= float(input("Qual será o tempo tde aplicação: "))
+taxa= float(input("Digite o valor taxa:"))
 juros= (capital*taxa*tempo)/100
-print("a taxa de juros é:",juros,"")
+print("a sua nova taxa de juros é:",juros,"")
