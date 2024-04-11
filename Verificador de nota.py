9e4114fa1302d15c1138266f457aa9581171929d
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-04-11 08:26:13 -0300

    Update Verificador de nota.py

--- Verificador de nota.py
+++ Verificador de nota.py
@@ -1,12 +1,12 @@
 print("Bom dia, hora de verificar suas notas")
-nota = float(input("Digite a sua nota "))
+nota = float(input("Digite a sua nota: "))
 if nota >=9:
-    print("Você é um exelente aluno")
+    print("Você é um exelente aluno,nota A")
 elif nota >=7.5:
-    print("Você está muito bom") 
+    print("Você está muito bom, nota B ") 
 elif nota >=6:
-    print("Você está evoluindo continue")
+    print("Você está evoluindo continue, nota C")
 elif nota >=4:
-    print ("Você está mau hora de estudar")
+    print ("Você está mau hora de estudar,nota D")
 else: 
-    print("Reprovado")
+    print("Você está reprovado,nota F")
