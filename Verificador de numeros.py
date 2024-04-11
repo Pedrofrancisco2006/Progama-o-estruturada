7e4d9a426cb5aab8c13694d24e999ec7cac364e3
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-04-11 09:10:40 -0300

    Update Verificador de numeros.py

--- Verificador de numeros.py
+++ Verificador de numeros.py
@@ -1,5 +1,7 @@
       #Verificador de número par,impar,positivo,negativo ou zero
 numero = float(input('Digite um número: '))
+
+        #Possíveis Casos
 if numero  > 0  and numero % 2 == 0 :
     print("Número é Positivo e Par")
 elif numero > 0 and numero % 3 == 0 :   
