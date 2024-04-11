e76f7db1a5bd044b10b3a9792f82e49cabc2a688
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-04-11 08:36:07 -0300

    Update Calculadora de Area e perimetro de um Retangulo.py

--- Calculadora de Area e perimetro de um Retangulo.py
+++ Calculadora de Area e perimetro de um Retangulo.py
@@ -1,5 +1,10 @@
+        #Calcular a área de um retângulo
 base = float(input("Digite a base: "))
 altura = float(input("Digite a altura: "))
 area = base*altura 
+
+        #Calcular o perímetro do retângulo
 perimetro = 2*(base+altura)
-print("o retangulo tem perimetro",perimetro,"com",area,"de area.")
+
+        #Informar seu perímetro e área
+print("O retângulo tem perímetro",perimetro,"com",area,"de área.")
