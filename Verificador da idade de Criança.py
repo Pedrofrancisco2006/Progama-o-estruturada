1f62ab58d7f9fa511013348ac343b629ffc0e936
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-05-02 08:41:51 -0300

    Verificador da idade de Criança.py

--- Verificador da idade de Criança.py
+++ Verificador da idade de Criança.py
@@ -1,8 +1,10 @@
     #Idade 
 idade = float(input("Digite a sua Idade: "))
-   
+
     #Classificação idade
-if idade <13:
+if idade <0:
+    print("Esta idade não pode ser usada")
+elif idade <13:
     print("Esta idade é de uma criança")
 elif idade <17:
     print("Esta idade é de um adolecente")
