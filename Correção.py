04f2f2cd769623829950be187853312e373a6f7f
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-06-13 11:06:07 -0300

    Add files via upload

---
+++ Correção.py
@@ -0,0 +1,27 @@
+comidas = ['leite' ,'couve' , 'tomate' , 'garfo' , 'faca' , 'refrigerante']
+bebidas = ['uva', 'colher ', 'vinho', 'cerveja ', 'banana']
+talheres = ['arroz' , 'concha' , 'whisky' ,'vodka' , 'feijão' , 'colher de chá']
+del comidas[3]
+del comidas[4]
+del comidas[3]
+comidas.append('uva')
+comidas.append('banana')
+comidas.append('arroz')
+comidas.append('feijão')
+del bebidas [1]
+del bebidas [3]
+del bebidas [0]
+bebidas.append('whisky')
+bebidas.append('leite')               
+bebidas.append('refrigerante')           
+bebidas.append('vodka')
+del talheres [0]
+del talheres [2]
+del talheres [2]
+del talheres [1]
+talheres.append("colher")
+talheres.append('garfo')
+talheres.append('Faca')
+print(comidas)
+print(bebidas)
+print(talheres)
